319d5b0f83bbcb5bf0b1cb34415ee5cee1c62c92
Hakula Chen <i@hakula.xyz>
2021-12-17 18:10:09 +0800

    feat(layout): implement a basic layout (wip)

--- src/views/RootLayout.vue
+++ src/views/RootLayout.vue
@@ -1,3 +1,25 @@
 <template>
-  <div class="flex justify-center h-screen p-8" />
+  <a-layout class="w-full h-screen overflow-y-auto">
+    <a-layout-header class="flex items-center justify-center">
+      <span class="text-2xl text-white title">{{ metaInfo.title }}</span>
+    </a-layout-header>
+    <a-layout-content />
+    <a-layout-footer class="text-center">
+      &copy; {{ currentYear }} {{ metaInfo.title }}
+      | Made with
+      <span class="text-red-600">&#9829;</span>
+      by {{ metaInfo.author }}
+    </a-layout-footer>
+  </a-layout>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const metaInfo = {
+  title: 'CareeCuz',
+  author: 'Hakula',
+};
+
+const currentYear = computed(() => new Date().getFullYear());
+</script>
