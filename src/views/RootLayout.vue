affd37b1f02f45cecffd6912ebe553e20ff691b7
Hakula Chen <i@hakula.xyz>
2021-12-19 16:08:41 +0800

    fix(layout): minor fix

--- src/views/RootLayout.vue
+++ src/views/RootLayout.vue
@@ -1,14 +1,14 @@
 <template>
   <a-layout class="w-full h-screen overflow-y-auto">
     <a-layout-header class="flex items-center justify-center">
-      <span class="text-2xl text-white title">{{ metaInfo.title }}</span>
+      <span class="text-2xl text-white title">{{ meta.title }}</span>
     </a-layout-header>
     <a-layout-content />
     <a-layout-footer class="text-center">
-      &copy; {{ currentYear }} {{ metaInfo.title }}
+      &copy; {{ meta.currentYear }} {{ meta.title }}
       | Made with
-      <span class="text-red-600">&#9829;</span>
-      by {{ metaInfo.author }}
+      <span class="text-red-600">&hearts;</span>
+      by {{ meta.author }}
     </a-layout-footer>
   </a-layout>
 </template>
@@ -16,10 +16,9 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 
-const metaInfo = {
+const meta = {
   title: 'CareeCuz',
   author: 'Hakula',
+  currentYear: computed(() => new Date().getFullYear()),
 };
-
-const currentYear = computed(() => new Date().getFullYear());
 </script>
