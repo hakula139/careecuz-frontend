7a21bcf18a81dc44f59d4db818753a75280a40f6
Hakula Chen <i@hakula.xyz>
2022-01-12 14:41:07 +0800

    feat(user): implement user auth logic with session

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,39 @@
 <script lang="ts" setup>
 // #region imports
 
+import { useRouter } from 'vue-router';
 import { Socket } from 'socket.io-client';
 
 import { TIMEOUT } from '@/configs';
-import { inject } from '@/composables';
+import { inject, openMessage } from '@/composables';
 import { useStore } from '@/store';
 import { PushUserInfo, Resp } from '@/types';
 
+const router = useRouter();
 const store = useStore();
 const socket = inject<Socket>('socket');
 
 // #endregion
 
-const pushUserInfo = (): void => {
+const pushUserInfo = (retryLimit = 3): void => {
   if (store.getters.isLoggedIn) {
-    const { userId, userToken } = store.state;
-    console.log('user info:', userId);
+    const { userId, token } = store.state;
+    console.log('pushing user info:', userId);
     socket.timeout(TIMEOUT).emit(
       'user:info',
       {
         userId,
-        userToken,
+        token,
       } as PushUserInfo,
       (err: Error, resp: Resp): void => {
-        if (err || resp.code !== 200) {
-          console.log('failed to push user info:', err ? err.message : resp.message);
+        if (err && retryLimit > 0) {
+          pushUserInfo(retryLimit - 1); // retry after timeout
+        } else if (err || resp.code !== 200) {
+          const errorMessage = err ? '请求超时' : resp.message;
+          console.log('failed to push user info:', errorMessage);
+          openMessage('error', `请重新登录: ${errorMessage}`);
+          store.commit('authReset');
+          router.push({ name: 'UserLoginPage' });
         }
       },
     );
